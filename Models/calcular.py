ef10eeb58e4a1637fc759bd156686c7b2608cd78
theborges23 <pedro.2.3@hotmail.com>
2021-07-23 11:11:45 -0300

    Parte inicial do projeto, apenas o escopo foi definido

---
+++ Models/calcular.py
@@ -0,0 +1,56 @@
+from random import randint
+
+class Calcular:
+
+    def __init__(self: object, dificuldade: int, /) -> None:
+        self.__dificuldade: int = dificuldade
+        self.__valor1: int = self._gerar_valor
+        self.__valor2: int = self._gerar_valor
+        self.__operacao: int = randint(1, 3) # 1 = somar, 2 = diminuir, 3 = multiplicar
+        self.__resultado: int = self._gerar_resultado
+
+    @property
+    def dificuldade(self: object) -> int:
+        return self.__dificuldade
+
+    @property
+    def valor1(self: object) -> int:
+        return self.__valor1
+
+    @property
+    def valor2(self: object) -> int:
+        return self.__valor2
+
+    @property
+    def operacao(self: object) -> int:
+        return self.__operacao
+
+    @property
+    def resultado(self:object) -> int:
+        return self.__resultado
+
+    def __str__(self: object) -> str:
+        op: str = ''
+        if self.operacao == 1:
+            op = 'somar'
+        elif self.operacao == 2:
+            op = 'diminuir'
+        elif self.operacao == 3:
+            op = 'multiplicar'
+        else:
+            op = 'operacao desconhecida'
+        return f'valor 1: {self.valor1} \nValor 2: {self.valor2} \nDificultade: {self.dificuldade} \nOperação: {op}'
+
+    @property
+    def _gerar_valor(self: object) -> int:
+        pass
+
+    @property
+    def _gerar_resultado(self: object) -> int:
+        pass
+
+    def checar_resultado(self: object, resposta: int) -> bool:
+        pass
+
+    def mostrar_operacao(self: object) -> None:
+        pass
